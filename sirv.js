607af5abf593ac280d0c1ed492c431c0ed4bf760
Rick Hawkins <rick@alchemydes.com>
2015-07-24 18:22:41 -0700

    Added Sirv JavaScript.

---
+++ sirv.js
@@ -0,0 +1,5 @@
+(function(s,i,r,v) {
+  v = i.createElement(r); v.async=1;
+  v.src = ('file:'==location.protocol?'http:':'') + '//scripts.sirv.com/sirv.js';
+  i.getElementsByTagName(r)[0].parentNode.appendChild(v);
+})(window, document, 'script');
